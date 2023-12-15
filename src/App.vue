cddc9d731a25729afbde96028a0f837317b658dd
fardis1 <fardis@automedia.de>
2023-12-15 15:02:40 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>My app</div>
+  <Header />
+  <div class="container">
+    <Balance :total="total" />
+    <IncomeExpenses />
+    <TransactionList :transactions="transactions" />
+    <AddTransaction />
+  </div>
+</template>
+<script setup>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+//Anything you want it to be reactive you wrap it in ref function:
+import { ref, computed } from "vue";
+const transactions = ref([
+  { id: 1, text: "Flower", amount: -19.99 },
+  { id: 2, text: "Salary", amount: 299.33 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+]);
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h3>History</h3>
+    <ul id="list" class="list">
+      <li
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :class="transaction.amount < 0 ? 'minus' : 'plus'"
+      >
+        {{ transaction.text }}
+        <span>{{
+          transaction.amount < 0
+            ? `- $
+            ${transaction.amount.toString().slice(1)}`
+            : transaction.amount
+        }}</span>
+        <button class="delete-btn">x</button>
+      </li>
+    </ul>
+  </div>
+</template>
+<!-- Option API -->
+<!-- <script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: "Flower", amount: -19.99 },
+        { id: 2, text: "Salary", amount: 299.33 },
+        { id: 3, text: "Book", amount: -10 },
+        { id: 4, text: "Camera", amount: 150 },
+      ],
+    };
+  },
+};
+</script> -->
+//
+<!-- Composition API -->
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
